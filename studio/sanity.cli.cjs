ae601f1f7cafe9d0bc6ca825f47c38857cde59f8
Alexander <sale@sancan.ru>
2026-05-10 00:34:18 +0300

    chore(studio): switch sanity.cli to .ts with hardcoded projectId/studioHost

--- studio/sanity.cli.cjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('sanity/cli').CliConfig} */
-module.exports = {
-  api: {
-    projectId: process.env.SANITY_STUDIO_PROJECT_ID,
-    dataset: process.env.SANITY_STUDIO_DATASET || "production"
-  }
-};
---
+++ studio/sanity.cli.ts
@@ -0,0 +1,8 @@
+/** @type {import('sanity/cli').CliConfig} */
+export default {
+  api: {
+    projectId: "n72tvosu",
+    dataset: "production"
+  },
+  studioHost: "alex-studio"
+};
